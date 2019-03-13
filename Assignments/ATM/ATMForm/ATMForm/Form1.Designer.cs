11f7adea55df82d35c634204723d3e98c2e36767
mabraham123 <33029013+mabraham123@users.noreply.github.com>
2019-03-13 15:28:17 +0000

    Add files via upload

---
+++ Assignments/ATM/ATMForm/ATMForm/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ATMForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DisplayView = new System.Windows.Forms.TextBox();
+            this.fLPNumbers = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // DisplayView
+            // 
+            this.DisplayView.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisplayView.Location = new System.Drawing.Point(13, 30);
+            this.DisplayView.Name = "DisplayView";
+            this.DisplayView.Size = new System.Drawing.Size(651, 38);
+            this.DisplayView.TabIndex = 2;
+            this.DisplayView.Text = "Insert Card";
+            // 
+            // fLPNumbers
+            // 
+            this.fLPNumbers.Location = new System.Drawing.Point(120, 119);
+            this.fLPNumbers.Name = "fLPNumbers";
+            this.fLPNumbers.Size = new System.Drawing.Size(407, 443);
+            this.fLPNumbers.TabIndex = 3;
+            this.fLPNumbers.Paint += new System.Windows.Forms.PaintEventHandler(this.fLPNumbers_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 639);
+            this.Controls.Add(this.fLPNumbers);
+            this.Controls.Add(this.DisplayView);
+            this.Name = "Form1";
+            this.Text = "ATM Project";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox DisplayView;
+        private System.Windows.Forms.FlowLayoutPanel fLPNumbers;
+    }
+}
+
---
+++ Assignments/ATM/ATMForm/ATMForm/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ATMForm
+{
+    public partial class Form1 : Form
+    {
+        private Account[] ac = new Account[3];
+        private ATM atm;
+
+        Button[] btn = new Button[10];
+        int rows, columns = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ac[0] = new Account(300, 1111, 111111);
+            ac[1] = new Account(750, 2222, 222222);
+            ac[2] = new Account(3000, 3333, 333333);
+
+            atm = new ATM(ac);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fLPNumbers_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int rows = 0;  rows < 10; rows++)
+            {
+                btn[rows] = new Button();
+                btn[rows].Text = rows.ToString();
+                fLPNumbers.Controls.Add(btn[rows]);
+            }
+        }
+    }
+}
